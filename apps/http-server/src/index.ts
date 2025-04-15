ab2ab02fecd3bf9e9458e0a16610a545036051f9
ayushbag <ayubag21@gmail.com>
2025-04-15 15:08:01 +0530

    added prisma and http-server init

---
+++ apps/http-server/src/index.ts
@@ -0,0 +1,36 @@
+import express from "express"
+import { prisma } from "@repo/db/index"
+
+const app = express();
+
+app.use(express.json())
+
+app.get("/", (req, res) => {
+    res.send("hi there")
+})
+
+app.post("/signup",async (req, res) => {
+    const username = req.body.username
+    const password = req.body.password
+
+    if (username || password) {
+        await prisma.user.create({
+            data: {
+                username,
+                password
+            }
+        }).then(() => {
+            return res.status(201).json({
+                message: "Content Added!"
+            })
+        }).catch((error: any) => {
+            console.log(error)
+        })
+    } else {
+        res.status(204).json({
+            messsage: "Give proper inputs!"
+        })
+    }
+})
+
+app.listen(3001, () => console.log("Server Started on PORT 3000"))
